0b0164183fd542554112b141c9ba86ee05d1a399
.email.com <gangisettymani98@gmail.com>
2026-07-08 16:15:34 +0530

    if i want create new instance code

--- 03-mysql.sh
+++ 03-mysql.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 LOGS_FOLDER="/var/log/roboshop"
-sudo mkdir -p $LoGS_FOLDER
-sudo chown -R ec2-user:ec2-user $LoGS_FOLDER
-sudo chmod -R 755 $LoGS_FOLDER
+sudo mkdir -p $LOGS_FOLDER
+sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
+sudo chmod -R 755 $LOGS_FOLDER
 LOGS_FILE="$LOGS_FOLDER/$0.log"
 
 USERID=$(id -u)
