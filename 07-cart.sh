c2d1dd8d371b3fa4064a530c9a3329a6e9eac188
.email.com <gangisettymani98@gmail.com>
2026-07-08 17:23:31 +0530

    if i want create new instance code

---
+++ 07-cart.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+LOGS_FILE="/var/log/roboshop"
+sudo mkdir -p $LOGS_FILE
+sudo chown -R ec2-user:ec2-user $LOGS_FILE
+sudo chmod -R 755 $LOGS_FILE
+LOGS_FILE="$LOGS_FILE/$0.log"
+SCRIPT_DIR=$PWD
+
+USERID=$(id -u)
+
+RED='\e[1;31m'
+GREEN='\e[1;32m'
+YELLOW='\e[1;33m'
+BLUE='\e[1;34m'
+NC='\e[0m' # No Color (Reset)
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+
+if [ $USERID -ne 0 ]; then
+ echo "$timestamp [ERROR] $GREEN please run this script with root access $NC" | tee -a $LOGS_FILE
+        exit 1
+fi
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$timestamp [ERROR] $2..............$BLUE FAILURE $NC" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$timestamp [INFO] $2..............$GREEN SUCCESS  $NC"  | tee -a $LOGS_FILE
+    fi
+}
+
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+VALIDATE $? "install nodejs :20"
+
+id roboshop
+if [ $? -ne 0 ]; then
+ 
+   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+       VALIDATE $? "createing roboshop system user"
+else
+    echo -e "system user roboshop alredy create......$BLUE Skipping $NC"
+fi
+
+mkdir -p  /app 
+VALIDATE $? "createing app directory"
+
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
+cd /app 
+unzip /tmp/cart.zip
+
+npm install 
+VALIDATE $? "Installing dependencies"
+
+cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
+VALIDATE $? "Created systemctl service"
+
+systemctl daemon-reload
+VALIDATE $ "daemon-reload cart"
+
+systemctl enable cart 
+systemctl restart cart 
+VALIDATE $? "Restarting cart"
+
+
+
+
+
+
